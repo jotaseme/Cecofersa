71638a4f11ca83e6662343a96514121e9d2659c3
jotaseme <jotaememoreno@gmail.com>
2017-03-13 11:39:51 +0100

    get formulario de actualizacion de asociado y creacion del metodo para recoger los datos por post

--- src/BackBundle/Controller/AsociadosController.php
+++ src/BackBundle/Controller/AsociadosController.php
@@ -2,8 +2,13 @@
 
 namespace BackBundle\Controller;
 
+use BackBundle\Form\AsociadosType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session\Session;
+use Symfony\Component\HttpFoundation\Response;
 
 class AsociadosController extends Controller
 {
@@ -45,6 +50,41 @@ class AsociadosController extends Controller
         return $this->render('Backoffice/Asociados/detalle_asociado.html.twig', [
             'asociado'=>$asociado
         ]);
+    }
+
+
+    /**
+     * @Route("/Admin/asociados/{id_asociado}/edit",name="edit_asociado", requirements={"id_asociado": "\d+"})
+     * @Method({"GET"})
+     */
+    public function editAsociadoAction($id_asociado)
+    {
+        $asociado = $this->getDoctrine()
+            ->getRepository('BackBundle:Asociados')
+            ->find($id_asociado);
+
+        $form_asociado = $this->createForm(AsociadosType::class, $asociado);
+        $form_asociado->get('provincia')->setData(ucfirst(strtolower($asociado->getProvincia())));
+
+        if(!$asociado){
+            throw $this->createNotFoundException(
+                'Error en la busqueda del asociado '
+            );
+        }
+        return $this->render('Backoffice/Asociados/edit_asociado.html.twig', [
+            'asociado'=>$asociado,
+            'form'=>$form_asociado->createView()
+        ]);
+
+    }
+
+    /**
+     * @Route("/Admin/asociados/{id_asociado}/edit",name="edit_post_asociado", requirements={"id_asociado": "\d+"})
+     * @Method({"POST"})
+     */
+    public function editPOSTAsociadoAction(Request $request)
+    {
+        die;
 
     }
 }
--- src/BackBundle/Form/AsociadosType.php
+++ src/BackBundle/Form/AsociadosType.php
@@ -3,8 +3,14 @@
 namespace BackBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Validator\Constraints\Date;
 
 class AsociadosType extends AbstractType
 {
@@ -13,7 +19,198 @@ class AsociadosType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('nombre')->add('domicilio')->add('poblacion')->add('codigoPostal')->add('provincia')->add('contacto')->add('telefono')->add('fax')->add('eMail')->add('numEmpleados')->add('numVendedores')->add('zonaInfluencia')->add('superficieTienda')->add('otrasEspec')->add('codigoAsociado')->add('soloPedirCitas')->add('logotipo')->add('supAlmacen')->add('pagWeb')->add('comunidadAutonoma')->add('nif')->add('pais')->add('accFeria')->add('activoFeria')->add('activo')->add('fechaAlta')->add('fechaBaja');
+        $builder
+            ->add('nombre', TextType::class, array(
+                'label'=> 'Nombre',
+                'required'=>'required',
+                'attr'=> array(
+                    'class'=> 'form-name form-control'
+                )
+            ))
+            ->add('domicilio', TextType::class, array(
+                'label'=> 'Direccion',
+                'required'=>'required',
+                'attr'=> array(
+                    'class'=> 'form-name form-control'
+                )
+            ))
+            ->add('poblacion', TextType::class, array(
+                'label'=> 'Población',
+                'required'=>'required',
+                'attr'=> array(
+                    'class'=> 'form-name form-control'
+                )
+            ))
+            ->add('codigoPostal', TextType::class, array(
+                'label'=> 'Código postal',
+                'required'=>'required',
+                'attr'=> array(
+                    'class'=> 'form-name form-control'
+                )
+            ))
+            ->add('provincia', ChoiceType::class, array(
+                'label'=> 'Provincia',
+                'required'=>'required',
+                'attr'=> array(
+                    'class'=> 'form-name form-control'
+                ),
+                'choices' => array(
+                    "Álava"=>"Álava",
+                    "Albacete"=>"Albacete",
+                    "Alicante"=>"Alicante",
+                    "Almería"=>"Almería",
+                    "Andorra"=>"Andorra",
+                    "Asturias"=>"Asturias",
+                    "Aveiro"=>"Aveiro",
+                    "Ávila"=>"Ávila",
+                    "Azores"=>"Azores",
+                    "Badajoz"=>"Badajoz",
+                    "Baleares"=>"Baleares",
+                    "Barcelona"=>"Barcelona",
+                    "Beja"=>"Beja",
+                    "Braga"=>"Braga",
+                    "Braganza"=>"Braganza",
+                    "Burgos"=>"Burgos",
+                    "Cáceres"=>"Cáceres",
+                    "Cádiz"=>"Cádiz",
+                    "Cantabria"=>"Cantabria",
+                    "Castellón"=>"Castellón",
+                    "Castelo Branco"=>"Castelo Branco",
+                    "Ceuta"=>"Ceuta",
+                    "Ciudad Real"=>"Ciudad Real",
+                    "Coimbra"=>"Coimbra",
+                    "Córdoba"=>"Córdoba",
+                    "Cuenca"=>"Cuenca",
+                    "Ávora"=>"Ávora",
+                    "Faro"=>"Faro",
+                    "Gerona"=>"Gerona",
+                    "Gibraltar"=>"Gibraltar",
+                    "Granada"=>"Granada",
+                    "Guadalajara"=>"Guadalajara",
+                    "Guarda"=>"Guarda",
+                    "Guipúzcoa"=>"Guipúzcoa",
+                    "Huelva"=>"Huelva",
+                    "Huesca"=>"Huesca",
+                    "Jaén"=>"Jaén",
+                    "La Coruña"=>"La Coruña",
+                    "La Rioja"=>"La Rioja",
+                    "Las Palmas de Gran Canaria"=>"Las Palmas de Gran Canaria",
+                    "Leiria"=>"Leiria",
+                    "León"=>"León",
+                    "Lérida"=>"Lérida",
+                    "Lisboa"=>"Lisboa",
+                    "Lugo"=>"Lugo",
+                    "Madeira"=>"Madeira",
+                    "Madrid"=>"Madrid",
+                    "Málaga"=>"Málaga",
+                    "Melilla"=>"Melilla",
+                    "Murcia"=>"Murcia",
+                    "Navarra"=>"Navarra",
+                    "Oporto"=>"Oporto",
+                    "Orense"=>"Orense",
+                    "Pontevedra"=>"Pontevedra",
+                    "Portalegre"=>"Portalegre",
+                    "Portugal"=>"Portugal",
+                    "Salamanca"=>"Salamanca",
+                    "Santa Cruz de Tenerife"=>"Santa Cruz de Tenerife",
+                    "Santarém"=>"Santarém",
+                    "Segovia"=>"Segovia",
+                    "Setúbal"=>"Setúbal",
+                    "Sevilla"=>"Sevilla",
+                    "Soria"=>"Soria",
+                    "Tarragona"=>"Tarragona",
+                    "Teruel"=>"Teruel",
+                    "Toledo"=>"Toledo",
+                    "Valencia"=>"Valencia",
+                    "Valladolid"=>"Valladolid",
+                    "Viana do Castelo"=>"Viana do Castelo",
+                    "Vila Real"=>"Vila Real",
+                    "Viseu"=>"Viseu",
+                    "Vizcaya"=>"Vizcaya",
+                    "Zamora"=>"Zamora",
+                    "Zaragoza"=>"Zaragoza"
+                )
+            ))
+            ->add('contacto', TextType::class, array(
+                'label'=> 'Contacto',
+                'required'=>'required',
+                'attr'=> array(
+                    'class'=> 'form-name form-control'
+                )
+            ))
+            ->add('telefono', TextType::class, array(
+                'label'=> 'Teléfono',
+                'required'=>'required',
+                'attr'=> array(
+                    'class'=> 'form-name form-control'
+                )
+            ))
+            ->add('fax',TextType::class, array(
+                'label'=> 'Fax',
+                'required'=>'required',
+                'attr'=> array(
+                    'class'=> 'form-name form-control'
+                )
+            ))
+            ->add('eMail', EmailType::class, array(
+                'label'=> 'Email',
+                'required'=>'required',
+                'attr'=> array(
+                    'class'=> 'form-name form-control'
+                )
+            ))
+            ->add('comunidadAutonoma', TextType::class, array(
+                'label'=> 'Comunidad autónoma',
+                'required'=>false,
+                'attr'=> array(
+                    'class'=> 'form-name form-control'
+                )
+            ))
+            ->add('nif', TextType::class, array(
+                'label'=> 'NIF',
+                'required'=>'required',
+                'attr'=> array(
+                    'class'=> 'form-name form-control'
+                )
+            ))
+            ->add('pais', ChoiceType::class, array(
+                'label'=> 'Pais',
+                'required'=>'required',
+                'attr'=> array(
+                    'class'=> 'form-name form-control'
+                ),
+                'choices' => array('España' => 'ES',
+                    'Portugal' =>'PT')
+            ))
+            ->add('activo', ChoiceType::class, array(
+                'label'=> 'Activo',
+                'required'=>'required',
+                'attr'=> array(
+                    'class'=> 'form-name form-control'
+                ),
+                'choices' => array('Activo' => '1',
+                    'Inactivo' =>'0')
+            ))
+
+            ->add('fechaAlta', DateType::class, array(
+                'label'=> 'Fecha de alta',
+                'attr'=> array(
+                    'class'=> 'form-date form-control'
+                )
+
+            ))
+            ->add('fechaBaja', DateType::class, array(
+                'label'=> 'Fecha de baja',
+                'attr'=> array(
+                    'class'=> 'form-date form-control'
+                )
+
+            ))
+            ->add('Actualizar asociado',SubmitType::class, array(
+                "attr" => array(
+                    "class"=>"form-submit btn btn-primary"
+                )
+            ));
     }
     
     /**
