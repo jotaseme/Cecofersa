74448c17b6ac50806f18d4a4a0c428905e254d82
frozito <c4l4m4res>
2017-03-21 13:16:14 +0100

    Menu con idiomas completo (controlador y plantilla)

--- src/AppBundle/Controller/HomeController.php
+++ src/AppBundle/Controller/HomeController.php
@@ -8,13 +8,98 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 class HomeController extends Controller
 {
     /**
-     * @Route("/{languaje}", name="index")
+     * @Route("/{languaje}", name="ceco_index")
      */
-    public function indexAction($languaje='es')
+    public function indexAction($languaje='es', $activo='index')
     {
-        return $this->render('Cecofersa/encabezado.html.twig',
-            ['languaje' => $languaje
+        return $this->render('Cecofersa/menu.html.twig',
+            ['languaje' => $languaje,
+             'activo' => $activo
         ]);
+
+    }
+
+    /**
+     * @Route("/{languaje}/somos", name="ceco_somos")
+     */
+    public function somosAction($languaje='es', $activo='somos')
+    {
+        return $this->render('Cecofersa/menu.html.twig',
+            ['languaje' => $languaje,
+             'activo' => $activo,
+            ]);
+
+    }
+
+    /**
+     * @Route("/{languaje}/servicios", name="ceco_servicios")
+     */
+    public function serviciosAction($languaje='es', $activo='servicios')
+    {
+        return $this->render('Cecofersa/menu.html.twig',
+            ['languaje' => $languaje,
+             'activo' => $activo
+            ]);
+
+    }
+
+    /**
+     * @Route("/{languaje}/folletos", name="ceco_folletos")
+     */
+    public function folletosAction($languaje='es', $activo='folletos')
+    {
+        return $this->render('Cecofersa/menu.html.twig',
+            ['languaje' => $languaje,
+             'activo' => $activo
+            ]);
+
+    }
+
+    /**
+     * @Route("/{languaje}/productos", name="ceco_productos")
+     */
+    public function productosAction($languaje='es', $activo='productos')
+    {
+        return $this->render('Cecofersa/menu.html.twig',
+            ['languaje' => $languaje,
+             'activo' => $activo
+            ]);
+
+    }
+
+    /**
+     * @Route("/{languaje}/informacion", name="ceco_informacion")
+     */
+    public function informacionAction($languaje='es', $activo='informacion')
+    {
+        return $this->render('Cecofersa/menu.html.twig',
+            ['languaje' => $languaje,
+             'activo' => $activo
+            ]);
+
+    }
+
+    /**
+     * @Route("/{languaje}/asociados", name="ceco_asociados")
+     */
+    public function asociadosAction($languaje='es', $activo='asociados')
+    {
+        return $this->render('Cecofersa/menu.html.twig',
+            ['languaje' => $languaje,
+             'activo' => $activo
+            ]);
+
+    }
+
+    /**
+     * @Route("/{languaje}/contacto", name="ceco_contacto")
+     */
+    public function contactoAction($languaje='es', $activo='contacto')
+    {
+        return $this->render('Cecofersa/menu.html.twig',
+            ['languaje' => $languaje,
+             'activo' => $activo
+            ]);
 
     }
 }
