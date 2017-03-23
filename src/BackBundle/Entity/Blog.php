6f5e2d891868ce66ee93c094dcd2ce6ac6cd0058
frozito <c4l4m4res>
2017-03-23 10:24:17 +0100

    Ceco-Productos acabada. Entities: generada Blog, regenerada DireccionesAsociados.

---
+++ src/BackBundle/Entity/Blog.php
@@ -0,0 +1,221 @@
+<?php
+
+namespace BackBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Blog
+ *
+ * @ORM\Table(name="blog")
+ * @ORM\Entity
+ */
+class Blog
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id-entrada", type="integer", nullable=false)
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $idEntrada;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="titulo", type="string", length=45, nullable=true)
+     */
+    private $titulo;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="subtitulo", type="string", length=100, nullable=true)
+     */
+    private $subtitulo;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="cuerpo", type="blob", length=65535, nullable=true)
+     */
+    private $cuerpo;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="fecha", type="datetime", nullable=true)
+     */
+    private $fecha;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="idioma", type="string", length=2, nullable=true)
+     */
+    private $idioma;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="imagen", type="string", length=45, nullable=true)
+     */
+    private $imagen;
+
+
+
+    /**
+     * Get idEntrada
+     *
+     * @return integer
+     */
+    public function getIdEntrada()
+    {
+        return $this->idEntrada;
+    }
+
+    /**
+     * Set titulo
+     *
+     * @param string $titulo
+     *
+     * @return Blog
+     */
+    public function setTitulo($titulo)
+    {
+        $this->titulo = $titulo;
+
+        return $this;
+    }
+
+    /**
+     * Get titulo
+     *
+     * @return string
+     */
+    public function getTitulo()
+    {
+        return $this->titulo;
+    }
+
+    /**
+     * Set subtitulo
+     *
+     * @param string $subtitulo
+     *
+     * @return Blog
+     */
+    public function setSubtitulo($subtitulo)
+    {
+        $this->subtitulo = $subtitulo;
+
+        return $this;
+    }
+
+    /**
+     * Get subtitulo
+     *
+     * @return string
+     */
+    public function getSubtitulo()
+    {
+        return $this->subtitulo;
+    }
+
+    /**
+     * Set cuerpo
+     *
+     * @param string $cuerpo
+     *
+     * @return Blog
+     */
+    public function setCuerpo($cuerpo)
+    {
+        $this->cuerpo = $cuerpo;
+
+        return $this;
+    }
+
+    /**
+     * Get cuerpo
+     *
+     * @return string
+     */
+    public function getCuerpo()
+    {
+        return $this->cuerpo;
+    }
+
+    /**
+     * Set fecha
+     *
+     * @param \DateTime $fecha
+     *
+     * @return Blog
+     */
+    public function setFecha($fecha)
+    {
+        $this->fecha = $fecha;
+
+        return $this;
+    }
+
+    /**
+     * Get fecha
+     *
+     * @return \DateTime
+     */
+    public function getFecha()
+    {
+        return $this->fecha;
+    }
+
+    /**
+     * Set idioma
+     *
+     * @param string $idioma
+     *
+     * @return Blog
+     */
+    public function setIdioma($idioma)
+    {
+        $this->idioma = $idioma;
+
+        return $this;
+    }
+
+    /**
+     * Get idioma
+     *
+     * @return string
+     */
+    public function getIdioma()
+    {
+        return $this->idioma;
+    }
+
+    /**
+     * Set imagen
+     *
+     * @param string $imagen
+     *
+     * @return Blog
+     */
+    public function setImagen($imagen)
+    {
+        $this->imagen = $imagen;
+
+        return $this;
+    }
+
+    /**
+     * Get imagen
+     *
+     * @return string
+     */
+    public function getImagen()
+    {
+        return $this->imagen;
+    }
+}
